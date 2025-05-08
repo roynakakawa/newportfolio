d6f0e0559374fcb2be1017e50fa23ee69964d196
Roy Nakakawa <roy.nakakawa@protonmail.com>
2025-05-08 10:34:09 +0200

    fixed typescript bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 
-const dayName = (date: string, locale: string) => date.toLocaleDateString(locale, { weekday: 'long' });
+const date = new Date();
+const formatDate = date.toLocaleDateString('en-US', { weekday: 'long' });
 
 export default function Home() {
   return (
     <div className="justify-items-center pb-content sm:mt-18">
       <main className="grid w-full sm:grid-cols-2 sm:max-w-1/2">
         <div className="text-center sm:text-left mb-12">
-          <h1 className="mb-6 text-lg font-bold vhs">Hello there! Happy {dayName(new Date())}</h1>
+          <h1 className="mb-6 text-lg font-bold vhs">Hello there! Happy {formatDate}</h1>
           <p className="mb-6 text-md vhsm">I'm Roy Nakakawa</p>
           <p className="text-sm vhsm">I've been delivering projects for about 15 years</p>
           <p className="text-sm vhsm">and I have over 6 years in the Blockchain ecosystem</p>
