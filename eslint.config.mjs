1f571f5db9678a986ec56af05a2bf5e4cfb9603f
Roy Nakakawa <roy.nakakawa@protonmail.com>
2025-05-08 10:41:53 +0200

    added eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,9 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  rules: {
-    'react/no-unescaped-entities': 'off',
-  },
+  compat.config({
+    rules: {
+      'react/no-unescaped-entities': 'off',
+    },
+  }),
 ];
 
 export default eslintConfig;
