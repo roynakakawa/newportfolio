0df5c3b29c26ed21dfb24a743194147c7f65ee23
Roy Nakakawa <roy.nakakawa@protonmail.com>
2025-05-13 08:38:34 +0200

    small footer fix

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
     return (
         <div className="absolute bottom-12 sm:bottom-0 inset-x-0 h-12 justify-items-center">
             <div className="flex  text-xs">
-                <a>© {new Date().getFullYear()} Crafted with ♥ nextjs + tailwind + Roy</a>
+                <a>© {new Date().getFullYear()} Crafted with ♥ by Roy</a>
             </div>
         </div>
     );
