f4799593154752db0858329a2418544f1793771a
Roy Nakakawa <roy.nakakawa@protonmail.com>
2025-05-13 12:41:41 +0200

    adding logos

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,6 +7,22 @@ export default function About() {
             I usually spend my time reading and having fun automating boring things. I've been involved in the Crypto ecosystem for about 6 years.
             I really believe that Blockchain can change our current understanding of how money works and empower individuals over centralized authorities.          
           </a>
+          <h1 className="text-sm font-bold mt-12 mb-12 vhs">
+            teams where I have collaborated:
+          </h1>
+          <div className="grid grid-cols-3 sm:max-w-1/2 gap-6">
+            <img className="glitch" src="./bitso.png" height="50" width="50" alt=""/>
+            <img className="glitch" src="./cardano.png" height="50" width="50" alt=""/>
+            <img className="glitch" src="./iohk.png" height="50" width="50" alt=""/>
+            <img className="glitch" src="./mina.png" height="50" width="50" alt=""/>
+            <img className="glitch" src="./mlabs.png" height="50" width="50" alt=""/>
+            <img className="glitch" src="./wanchain.png" height="40" width="40" alt=""/>
+          </div>
+          <h1 className="text-sm font-bold mt-12 mb-6 vhs">
+
+          </h1>
+          <div className="grid grid-cols-3 sm:max-w-1/2">
+          </div>
         </main>
       </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="justify-items-center pb-content sm:mt-18">
       <main className="grid w-full sm:grid-cols-2 sm:max-w-1/2">
         <div className="text-center sm:text-left mb-12">
-          <h1 className="mb-6 text-lg font-bold vhs">Hello there!</h1>
+          <h1 className="mb-6 text-lg font-bold vhs">Hello there! happy day</h1>
           <p className="mb-6 text-md vhsm">I'm Roy Nakakawa</p>
           <p className="text-sm vhsm">I've been delivering projects for about 15 years</p>
           <p className="text-sm vhsm">and I have over 6 years in the Blockchain ecosystem</p>
         </div>
         <div className="justify-items-center">
-          <Image
-          src="/royito.png"
-          alt="Roy"
-          className="glitch"
-          width= {140}
-          height={140}
-          />
+          <img className="glitch saturate-55" src="./royito.png" height="140" width="140" alt="Roy"/>
         </div>
       </main>
     </div>
