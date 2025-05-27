75e5225350d8ea24a37a12e490f3346bd0feaea8
Roy Nakakawa <roy.nakakawa@protonmail.com>
2025-05-27 12:17:03 +0200

    adding projects and some styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ export default function About() {
             I usually spend my time reading and having fun automating boring things. I've been involved in the Crypto ecosystem for about 6 years.
             I really believe that Blockchain can change our current understanding of how money works and empower individuals over centralized authorities.          
           </a>
-          <h1 className="text-sm font-bold mt-18 sm:mt-12 mb-8 sm:mb-10 text-center sm:text-left vhs">
+          <h1 className="text-sm font-bold mt-18 sm:mt-16 mb-8 sm:mb-4 text-center sm:text-left vhs">
             teams where I have collaborated:
           </h1>
           <div className="grid grid-cols-3 sm:grid-cols-6 gap-6 justify-items-center sm:justify-items-start">
@@ -18,64 +18,72 @@ export default function About() {
             <img className="glitch" src="./mlabs.png" height="50" width="50" alt=""/>
             <img className="glitch" src="./wanchain.png" height="40" width="40" alt=""/>
           </div>
-          <h1 className="text-sm font-bold mt-18 sm:mt-12 mb-8 sm:mb-10 vhs">
+          <h1 className="text-sm font-bold mt-18 sm:mt-16 mb-8 sm:mb-4 vhs">
             my skills
           </h1>
-          <div className="grid grid-cols-3 sm:grid-cols-5 gap-6 sm:gap-14">
+          <div className="grid grid-cols-3 sm:grid-cols-5 gap-6 sm:gap-x-12 sm:gap-y-2 ">
             <div className="grid grid-rows-2 text-xs">
               <a className="vhsm">delivery</a>
-              <div className="flex h-4 overflow-hidden rounded text-xs border-1 glitch">
+              <div className="flex h-3 overflow-hidden rounded text-xs border-1 glitch">
                 <div className="bg-(--foreground2) w-9/10 text-(--background) glitch"></div>
               </div>
             </div>
             <div className="grid grid-rows-2 text-xs">
               <a className="vhsm">agile</a>
-              <div className="flex h-4 overflow-hidden rounded text-xs border-1 glitch">
+              <div className="flex h-3 overflow-hidden rounded text-xs border-1 glitch">
                 <div className="bg-(--foreground2) w-8/10 text-(--background) glitch"></div>
               </div>
             </div>
             <div className="grid grid-rows-2 text-xs">
               <a className="vhsm">operations</a>
-              <div className="flex h-4 overflow-hidden rounded text-xs border-1 glitch">
+              <div className="flex h-3 overflow-hidden rounded text-xs border-1 glitch">
                 <div className="bg-(--foreground2) w-9/10 text-(--background) glitch"></div>
               </div>
             </div>
             <div className="grid grid-rows-2 text-xs">
               <a className="vhsm">blockchain</a>
-              <div className="flex h-4 overflow-hidden rounded text-xs border-1 glitch">
+              <div className="flex h-3 overflow-hidden rounded text-xs border-1 glitch">
                 <div className="bg-(--foreground2) w-8/10 text-(--background) glitch"></div>
               </div>
             </div>
             <div className="grid grid-rows-2 text-xs">
               <a className="vhsm">dApps</a>
-              <div className="flex h-4 overflow-hidden rounded text-xs border-1 glitch">
+              <div className="flex h-3 overflow-hidden rounded text-xs border-1 glitch">
                 <div className="bg-(--foreground2) w-8/10 text-(--background) glitch"></div>
               </div>
             </div>
             <div className="grid grid-rows-2 text-xs">
               <a className="vhsm">deFi</a>
-              <div className="flex h-4 overflow-hidden rounded text-xs border-1 glitch">
+              <div className="flex h-3 overflow-hidden rounded text-xs border-1 glitch">
                 <div className="bg-(--foreground2) w-7/10 text-(--background) glitch"></div>
               </div>
             </div>
             <div className="grid grid-rows-2 text-xs">
               <a className="vhsm">l2 scaling</a>
-              <div className="flex h-4 overflow-hidden rounded text-xs border-1 glitch">
+              <div className="flex h-3 overflow-hidden rounded text-xs border-1 glitch">
                 <div className="bg-(--foreground2) w-8/10 text-(--background) glitch"></div>
               </div>
             </div>
             <div className="grid grid-rows-2 text-xs">
               <a className="vhsm">cex</a>
-              <div className="flex h-4 overflow-hidden rounded text-xs border-1 glitch">
+              <div className="flex h-3 overflow-hidden rounded text-xs border-1 glitch">
                 <div className="bg-(--foreground2) w-7/10 text-(--background) glitch"></div>
               </div>
             </div>
             <div className="grid grid-rows-2 text-xs">
               <a className="vhsm">zK</a>
-              <div className="flex h-4 overflow-hidden rounded text-xs border-1 glitch">
+              <div className="flex h-3 overflow-hidden rounded text-xs border-1 glitch">
                 <div className="bg-(--foreground2) w-7/10 text-(--background) glitch"></div>
               </div>
             </div>
+          </div>
+          <h1 className="text-sm font-bold mt-18 sm:mt-16 mb-8 sm:mb-4 vhs">
+            highlight projects
+          </h1>
+          <div className="grid grid-cols-3 gap-6 justify-items-center sm:max-w-1/2 sm:justify-items-start">
+            <img className="glitch" src="./mithril.png" height="50" width="50" alt=""/>
+            <img className="glitch" src="./hydra.png" height="50" width="50" alt=""/>
+            <img className="glitch" src="./singularity.png" height="50" width="50" alt=""/>
           </div>       
         </main>
       </div>
--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-        <div className="grid pb-content h-10 content-center justify-items-center bg-(--background)">
+        <div className="grid pb-content h-10 content-center justify-items-center">
             <div className="text-xs">
                 <a>© {new Date().getFullYear()} Crafted with ♥ by Roy</a>
             </div>
