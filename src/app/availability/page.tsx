18e6cf569bc108c12eb0acea0ea660171e3d6473
Roy Nakakawa <roy.nakakawa@protonmail.com>
2026-04-09 10:05:51 +0200

    updating current availability

--- src/app/availability/page.tsx
+++ src/app/availability/page.tsx
@@ -6,8 +6,17 @@ export default function Availability() {
             my current availability :
           </h1>
           <a className="text-sm vhsm">
-            I'm currently available: 1 FTE / 40 hours per week         
-          </a>    
+            I'm currently available: 0.5 FTE / 20 hours per week         
+          </a>
+          <h1 className="text-sm font-bold mb-4 mt-18 sm:mt-16 text-center sm:text-left vhs">
+            collaboration options :
+          </h1>
+          <a className="text-sm vhsm">
+            Consulting / Delivery Support: €100-€150/hour depending on scope.         
+          </a> 
+          <a className="text-sm vhsm">
+            Fractional Program Leadership: Custom engagement depending on scope.       
+          </a>       
         </main>
       </div>
     );
